4230dd17617827182adbabf38ef33bc3128d0bd0
cjqhenry14 <cjqhenry@gmail.com>
2015-12-22 23:53:01 -0500

    fixing multiple tables' query bug

--- orc_fdw.c
+++ orc_fdw.c
@@ -523,8 +523,8 @@ simIterateForeignScan(ForeignScanState *node)
     else {//region
         //ss[1][0] = '1';
     }
-
-    itoa(ss[1], tupledes->attrs[2]->atttypmod);
+    if(tupledes->attrs[2]->atttypmod >= 0)
+        itoa(tupledes->attrs[2]->atttypmod, ss[1]);
 
     Datum *columnValues = slot->tts_values;
     bool *columnNulls = slot->tts_isnull;
