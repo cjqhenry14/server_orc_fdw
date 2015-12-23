442fdb0ef6e6a68949b8eba84ba710662c5d94f9
cjqhenry14 <cjqhenry@gmail.com>
2015-12-22 21:53:56 -0500

    fixing multiple tables' query bug

--- orc_fdw.c
+++ orc_fdw.c
@@ -474,7 +474,7 @@ simIterateForeignScan(ForeignScanState *node)
 
     ExecClearTuple(slot);
 
-    char *ss[5] = {"1", "mike", "23", "hehe", "2013-01-01"};
+    char ss[5][15] = {"1", "mike", "23", "hehe", "2013-01-01"};
     ss[1][0] = 'N';
     //nation: int, string, int, string
     //region: int, string, string
