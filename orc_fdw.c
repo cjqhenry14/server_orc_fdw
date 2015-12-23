ad7698f98682547b4c89df958f97200d1d82a3ae
cjqhenry14 <cjqhenry@gmail.com>
2015-12-23 14:45:51 -0500

    fixing multiple tables' query bug

--- orc_fdw.c
+++ orc_fdw.c
@@ -547,7 +547,7 @@ simIterateForeignScan(ForeignScanState *node)
     for(i = 0; i < colNum; i++) {
         Datum columnValue = 0;
 
-        if(tmpNextTuple[i][0]=='l' && tmpNextTuple[i][1]=='a') {
+        if(tmpNextTuple[i][0]=='l') {
         //if(i==2 && colNum == 3) {
         //if(i==2) {//不报错,但是没有结果
             columnValue = InputFunctionCall(&orcState->in_functions[i],
