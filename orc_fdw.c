b115237fb84789d4f49baf3c4c21b6190bde0937
cjqhenry14 <cjqhenry@gmail.com>
2015-12-25 18:01:10 -0500

    fixing exist bug doing TPCH like querys.

--- orc_fdw.c
+++ orc_fdw.c
@@ -571,17 +571,10 @@ simIterateForeignScan(ForeignScanState *node)
     for(i = 0; i < colNum; i++) {
         Datum columnValue = 0;
 
-        if(colNum == 4 && (i == 1 || i == 3)) {
-            columnValue = InputFunctionCall(&orcState->in_functions[i],
+        columnValue = InputFunctionCall(&orcState->in_functions[i],
                                             ss[i], orcState->typioparams[i],
                                             tupledes->attrs[i]->atttypmod);
 
-        }
-        else {
-            columnValue = InputFunctionCall(&orcState->in_functions[i],
-                                            tmpNextTuple[i], orcState->typioparams[i],
-                                            tupledes->attrs[i]->atttypmod);
-        }
 
         slot->tts_values[i] = columnValue;
     }
